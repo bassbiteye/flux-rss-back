e18bf3f7333cd745cd771227836841ff511ac68d
bassirou.biteye <bassirou.biteye@atos.net>
2022-04-20 17:51:04 +0000

    first commit

---
+++ src/main/java/com/example/rssback/RssBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.rssback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RssBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rssback/RssBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rssback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
